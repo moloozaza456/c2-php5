969fb567d12d80b3b1d45505f138b4defd1cdfd9
Dumrong <dumdumrong13@gmail.com>
2021-12-03 11:34:26 +0700

    initail commit

---
+++ php501.php
@@ -0,0 +1,30 @@
+<?php
+	$season = array("summer","winter","sprint","autumn");
+	$count = count($season);
+	for($i = 0;$i<=$count-1; $i++){
+		echo $season[$i];
+		echo "<br />";
+	};
+?>
+
+
+<!-- //country = array("Thailand","Malaysia","Singapore")
+// $country = array("Thailand","Malaysia","Singapore");
+// $country[0] = "Thailand";
+// $country[1] = "Malaysia";
+// $country[2] = "Singapore";
+
+// echo "<br />";
+// echo "==========================================";
+
+// echo "<br />";
+// echo $country[0];
+// echo "<br />";
+// echo $country[1];
+// echo "<br />";
+// echo $country[2]; -->
+
+
+
+
+
---
+++ php503.php
@@ -0,0 +1,9 @@
+<?php
+	$salary = array("Mark" => "35000","Jhon" => "45000","Jeff" => "55000");
+	// echo $salary["Mark"]."<br/>";
+	// echo $salary["Jhon"]."<br/>";
+	// echo $salary["Jeff"]."<br/>";
+	for($i = 0;$i<=2;$i++){
+		echo $salary['Mark']."<br/>";
+	}
+?>
